d1758a9bcda64a51f0973c8ad9002cb76d62f29a
Оля Ярош <olya.yarosshh@gmail.com>
2024-05-29 18:45:11 +0300

    Implementation of the function myBlend(arr)

---
+++ file_js_homework6.js
@@ -0,0 +1,14 @@
+function myBlend(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("До перемішування:", arr);
+myBlend(arr);
+console.log("Після перемішування:", arr);
